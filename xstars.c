782fdd19318ea8269b23efe19867277f63703800
Philip J Freeman <elektron@halo.nu>
2014-06-27 02:49:41 -0700

    get root window geometry

--- xstars.c
+++ xstars.c
@@ -5,9 +5,6 @@
 
 #include "starslib.h"
 
-#define WIDTH 1024
-#define HEIGHT 768
-
 int main()
 {
 	// Open the display
@@ -15,35 +12,32 @@ int main()
 	Display *dpy = XOpenDisplay(NULL);
 	assert(dpy);
 
-
-	int whiteColor = WhitePixel(dpy, DefaultScreen(dpy));
-
-	// Get the root window
+	// Get the root window and attributes
 
 	Window w = DefaultRootWindow(dpy);
 
-	// Initialize the stars lib
-	struct universe* u;
-
-	new_universe( &u, WIDTH, HEIGHT, 512 );
-
+	XWindowAttributes w_attr;
+	assert(XGetWindowAttributes(dpy, w, &w_attr));
 
 	// Create a "Graphics Context"
 
+	int whiteColor = WhitePixel(dpy, DefaultScreen(dpy));
 	GC gc = XCreateGC(dpy, w, 0, NULL);
-
-	// Tell the GC we draw using the white color
-
 	XSetForeground(dpy, gc, whiteColor);
 
+	// Initialize the stars lib
+
+	struct universe* u;
+	new_universe( &u, w_attr.width, w_attr.height, 512 );
+
 	FRAME:
-	XClearArea( dpy, w, 0, 0, WIDTH, HEIGHT, 0 );
+	XClearArea( dpy, w, 0, 0, w_attr.width, w_attr.height, 0 );
 	for (;;) {
 		struct return_point rp;
 		int return_code = process_point( u, &rp  );
 		if (return_code == 0) { break; }
 		if (return_code == 1) {
-			XDrawPoint( dpy, w, gc, rp.x + (WIDTH/2), rp.y + (HEIGHT/2));
+			XDrawPoint( dpy, w, gc, rp.x + (w_attr.width/2), rp.y + (w_attr.height/2));
 		}
 	}
 	XFlush(dpy);
